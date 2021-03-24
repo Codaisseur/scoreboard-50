71c70d78388094a4b5c3f0572100ededea71572c
Matias Garcia <matiasigarcia91@gmail.com>
2021-03-24 15:47:42 +0100

    finish increase score

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 
 const Player = props => {
+  const increase = () => {
+    console.log("pressed player", props);
+    props.increaseScore(props.id);
+  };
   return (
     <div style={{ display: "flex" }}>
       <div style={{ marginRight: 30 }}>
         {props.name}: {props.score}
       </div>
-      {/* <button onClick={increase}>+</button> */}
+      <button onClick={increase}>+</button>
     </div>
   );
 };
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -2,23 +2,61 @@ import React, { useState } from "react";
 import Player from "./Player";
 
 const initialState = [
-  { name: "Matias", score: 1 },
-  { name: "Karla", score: 10 },
-  { name: "David", score: 8 },
+  { name: "Matias", score: 1, id: 1 },
+  { name: "Karla", score: 10, id: 2 },
+  { name: "David", score: 8, id: 3 },
 ];
 
 const Scoreboard = () => {
-  const [players, setPlayer] = useState(initialState);
+  const [players, setPlayers] = useState(initialState);
+  const [sortBy, setSortBy] = useState("score");
 
-  const sortedPlayers = [...players].sort((a, b) => b.score - a.score);
+  // callback prop
+  const increaseScore = id => {
+    const updatedPlayers = players.map(p => {
+      if (p.id === id) {
+        return { ...p, score: p.score + 1 };
+      } else {
+        return p;
+      }
+    });
+    setPlayers(updatedPlayers);
+  };
 
-  console.log(players);
+  const onChangeSort = event => {
+    console.log("value of select", event.target.value);
+    setSortBy(event.target.value);
+  };
+
+  let sortedPlayers = [...players];
+
+  if (sortBy === "score") {
+    sortedPlayers.sort((a, b) => b.score - a.score);
+  } else {
+    sortedPlayers.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
   return (
     <div>
       <h1>Codaisseur Scoreboard</h1>
       <div>
+        <label>Sort by:</label>
+        <select value={sortBy} onChange={onChangeSort}>
+          <option value={"score"}>Score</option>
+          <option value={"name"}>Name</option>
+        </select>
+      </div>
+      <div>
         {sortedPlayers.map((guy, index) => {
-          return <Player name={guy.name} score={guy.score} key={index} />;
+          return (
+            <Player
+              name={guy.name}
+              score={guy.score}
+              key={index}
+              id={guy.id}
+              increaseScore={increaseScore}
+            />
+          );
         })}
       </div>
     </div>
